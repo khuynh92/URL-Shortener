3ae0e3be12c5c2d879c6407b53e63c813b5384e9
Khoa Huynh <khoawin92@gmail.com>
2018-11-07 17:19:04 -0800

    inital tests and working get/post

---
+++ src/app.js
@@ -0,0 +1,51 @@
+import express from 'express';
+
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/404.js';
+import noAuth from './middleware/401.js';
+import noBody from './middleware/400.js';
+import conflict from './middleware/409.js';
+
+import router from './routes/router.js';
+
+let app = express();
+
+//dev & parser middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+//routes middleware
+app.use(router);
+
+//error middleware
+app.use(notFound);
+app.use(noAuth);
+app.use(conflict);
+app.use(noBody);
+app.use(errorHandler);
+
+let server = false;
+
+module.exports = {
+  start: (port) => {
+    if(!server) {
+      server = app.listen(port, (err) => {
+        if(err) { throw err; }
+        console.log('Server running on ' + port);
+      });
+    } else {
+      console.log('Server is already running');
+    }
+  },
+  stop: () => {
+    server.close(() => {
+      console.log('Server has closed');
+    });
+  },
+  server: app,
+};
